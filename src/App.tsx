c4964995d1b8cd33d1df08bcbc9bf38ae0a2ae15
Marco Santana <marcos.maio96@gmail.com>
2023-04-11 20:08:27 -0300

    react-router-dom configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/defaults';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>Hello World.</h1>
-
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Reprehenderit id veritatis eaque itaque veniam obcaecati similique
-          deleniti nisi minus, adipisci laudantium eius. Incidunt harum esse
-          omnis sit quaerat dignissimos nesciunt!
-        </p>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
