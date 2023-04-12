48c9273ae7e3446fb18d99d6524957c4461af5dd
Marco Santana <marcos.maio96@gmail.com>
2023-04-11 22:41:15 -0300

    Default layout initial setup

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  border: 1px solid red;
+
+  max-width: 70rem;
+  margin: 0 auto;
+  padding: 2rem 0;
+
+  display: flex;
+  flex-direction: column;
+`;
