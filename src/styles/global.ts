3f4be67f1e56a9cde6ed3528d321b5e790c64ef5
Marco Santana <marcos.maio96@gmail.com>
2023-04-25 16:09:44 -0300

    Scroll styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,21 @@ export const GlobalStyle = createGlobalStyle`
         background-color: ${(props) => props.theme['white-300']};
         color: ${(props) => props.theme['gray-700']};
         -webkit-font-smoothing: antialiased !important;
+
+        overflow-y: auto;
+        overflow-x: hidden;
+        &::-webkit-scrollbar {
+            width: 12px;
+        }
+
+        &::-webkit-scrollbar-track {
+            border-radius: 20px;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color: ${(props) => props.theme['gray-500']};
+            border-radius: 20px;
+        }
     }
 
     body, input, textarea, button {
